bde2329e1be4500b7992e752a58c829ae9eb179c
matheusht <dev.matheustheodoro@gmail.com>
2022-09-29 09:43:43 -0300

    feat: 🎸 SearchResults + ProductItem component addition

---
+++ components/SearchResults.tsx
@@ -0,0 +1,35 @@
+import { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+    results: Array<{
+        id: number;
+        price: number;
+        title: string;
+    }>
+    onAddToWishlist: (id: number) => void
+}
+
+export function SearchResults({ results, onAddToWishlist }: SearchResultsProps) {
+    const totalPrice = useMemo(() => {
+        return results.reduce((total, product) => {
+            return total + product.price;
+        }, 0)
+    }, [results])
+
+    return (
+        <div>
+            <h2>{totalPrice}</h2>
+
+            {results.map(product => {
+                return (
+                    <ProductItem
+                        key={product.id}
+                        product={product}
+                        onAddToWishList={onAddToWishlist}
+                    />
+                );
+            })}
+        </div>
+    );
+}
